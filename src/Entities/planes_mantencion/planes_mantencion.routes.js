741d749e9b5021e20b872885149711e14b4e3ea4
masteronetto <Da.onetto@duocuc.cl>
2026-06-02 00:01:47 +0000

    feat: update arriendo

--- src/Entities/planes_mantencion/planes_mantencion.routes.js
+++ src/Entities/planes_mantencion/planes_mantencion.routes.js
@@ -14,6 +14,9 @@ const router = require('express').Router();
 // CRUD de Planes
 router.post('/', crearPlan);
 router.get('/', obtenerTodosLosPlanes);
+// Obtener plan de una máquina
+router.get('/maquina/:maquina_id', obtenerPlanDeMaquina);
+
 router.get('/:id', obtenerPlanPorId);
 router.put('/:id', actualizarPlan);
 router.delete('/:id', eliminarPlan);
@@ -21,8 +24,5 @@ router.delete('/:id', eliminarPlan);
 // Asignación de planes a máquinas
 router.post('/:id/asignar-maquina/:maquina_id', asignarPlanAMaquina);
 router.delete('/:id/desasignar-maquina/:maquina_id', desasignarPlanDeMaquina);
-
-// Obtener plan de una máquina
-router.get('/maquina/:maquina_id', obtenerPlanDeMaquina);
 
 module.exports = router;
